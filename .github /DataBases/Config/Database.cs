88a26b10b74af676169b665c482208285733b9f7
len4uk <lenya1.3@mail.ru>
2020-06-26 02:16:03 +0300

    Add files via upload

---
+++ .github /DataBases/Config/Database.cs
@@ -0,0 +1,11 @@
+﻿namespace DataBases.Config
+{
+    public class Database
+    {
+        public string Address { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string DatabaseName { get; set; }
+        public int Port { get; set; }
+    }
+}
