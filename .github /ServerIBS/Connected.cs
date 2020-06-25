99cf789c72e392e196e09c8eacd3a30216c598f2
len4uk <lenya1.3@mail.ru>
2020-06-26 02:29:38 +0300

    Add files via upload

---
+++ .github /ServerIBS/Connected.cs
@@ -0,0 +1,63 @@
+﻿using ServerIBS.ConnectedPhones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServerIBS
+{
+    public class Connected
+    {
+        public string Result { get; set; }
+        public List<string> ListResult { get; set; }
+
+        public Connected(string text)
+        {
+            List<string> massTextClient = text.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            if (massTextClient[0] == "1")
+            {
+                massTextClient.ToList().RemoveAt(0);
+                ControllerConnected(massTextClient);
+            }
+               
+
+            if (massTextClient[0] == "2")
+            {
+               massTextClient.RemoveAt(0);
+                PhoneConnected(massTextClient);
+            }
+               
+
+        }
+
+        private void PhoneConnected(List<string> massTextClient)
+        {
+            ConnectPhone phone = new ConnectPhone();
+
+            string switcher = massTextClient[0];
+            massTextClient.RemoveAt(0);
+
+            Result = null;
+            ListResult = null;
+
+            switch (switcher)
+            {
+                case "regs":  Result = phone.Regs(massTextClient); break;
+                case "login": Result = phone.Login(massTextClient); break;
+                case "city": ListResult = phone.City(massTextClient);  break;
+                case "route": ListResult = phone.Route(massTextClient); break;
+            }
+
+            if (ListResult != null)
+                ListResult[ListResult.Count - 1] += "&&&";
+            else if (Result != null)
+                Result += "&&&";
+
+        }
+
+        private void ControllerConnected(List<string> massTextClient)
+        {
+          
+        }
+    }
+}
---
+++ .github /ServerIBS/Program.cs
@@ -0,0 +1,211 @@
+﻿using API.Config;
+using DataBases.Database;
+using Logger;
+using ServerIBS.Server;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerIBS
+{
+    class Program
+    {
+        // Thread signal.  
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public static int Count;
+        static void Main(string[] args)
+        {
+            #region Подключение к БД
+            ServerConfiguration.Instance.LoadAllSettings();
+            if (ConnectToDatabase(ServerConfiguration.Instance.ServerConfig))
+                LogManager.Write("Успешное подключение к БД", LogLevel.Info);
+            else
+            {
+                LogManager.Write("Не удалось подключится к БД", LogLevel.Error);
+            }
+
+            #endregion
+            
+            StartListening();
+        }
+
+        public static void StartListening()
+        {
+            // Establish the local endpoint for the socket.  
+            // The DNS name of the computer  
+            // running the listener is "host.contoso.com".  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList.Last();
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.  
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.  
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+
+                    // Wait until a connection is made before continuing.  
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.  
+            allDone.Set();
+
+            // Get the socket that handles the client request.  
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.  er
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.GetEncoding(0).GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read   
+                // more data.  dv
+                content = state.sb.ToString();
+                Console.WriteLine(content);
+                Connected connected = new Connected(content);
+
+                if (!String.IsNullOrEmpty(connected.Result))
+                {
+                    Count = 0;
+                    // All the data has been read from the   
+                    // client. Display it on the console.  
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                    // Echo the data back to the client.  
+                    Send(handler, connected.Result);
+                }
+                
+                else if(connected.ListResult.Count != 0)
+                {
+                    
+                    int count = 0;
+                    foreach(var text in connected.ListResult)
+                    {
+                       
+                        Count = connected.ListResult.Count - count;
+                        count++;
+                        // All the data has been read from the   
+                        // client. Display it on the console.  
+                        Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                            content.Length, content);
+                        // Echo the data back to the client.  
+                        Send(handler, text);
+                    }
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.GetEncoding("windows-1251").GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                if(Count == 0)
+                {
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static bool ConnectToDatabase(ServerConfig serverConfig)
+        {
+            LogManager.Write("Подключение к БД", LogLevel.Info);
+            Database.Instance.SetConnectionString(
+                serverConfig.Database.User,
+                serverConfig.Database.Password,
+                serverConfig.Database.DatabaseName,
+                serverConfig.Database.Address,
+                (uint)serverConfig.Database.Port);
+
+            bool success = Database.Instance.CheckConnection();
+
+            if (!success)
+            {
+                LogManager.Write("Ошибка подключения к БД", LogLevel.Info);
+            }
+
+            return success;
+        }
+
+    }
+}
