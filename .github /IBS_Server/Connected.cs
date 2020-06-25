57e11592b3f485d5f7807e908aec633d214b6d0b
len4uk <lenya1.3@mail.ru>
2020-06-26 02:18:59 +0300

    Add files via upload

---
+++ .github /IBS_Server/Connected.cs
@@ -0,0 +1,41 @@
+﻿using IBS_Server.ConnectedPhones;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBS_Server
+{
+    public class Connected
+    {
+        public string Result { get; set; }
+
+        public Connected(string text)
+        {
+            string[] massTextClient = text.Split(new char[] { '|' });
+
+            if (massTextClient[0] == "2")
+                ControllerConnected(massTextClient);
+
+            if (massTextClient[0] == "2")
+                PhoneConnected(massTextClient);
+
+        }
+
+        private void PhoneConnected(string[] massTextClient)
+        {
+            ConnectPhone phone = new ConnectPhone();
+
+            string[] switcher = massTextClient[1].Split(new char[] { ':' });
+
+            switch (switcher[0])
+            {
+                case "login": Result = phone.Login(massTextClient); break;
+            }
+        }
+
+        private void ControllerConnected(string[] massTextClient)
+        {
+          
+        }
+    }
+}
---
+++ .github /IBS_Server/StateObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace IBS_Server
+{
+    // Объект состояния для асинхронного считывания клиентских данных  
+    public class StateObject
+    {
+        // Client  socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer 8k.  
+        public const int BufferSize = 8192;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+}
