7bfb3e341dd20c53315c796eef1a895542aa0df4
len4uk <lenya1.3@mail.ru>
2020-06-26 02:11:34 +0300

    Add files via upload

---
+++ .github /API/Константы/FileName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Constants
+{
+    public static class FileName
+    {
+        /// <summary>Название файла базы данных</summary>
+        public static readonly string ContentDatabase = "Local.sdf";
+               
+        public static class Server
+        {
+            /// <summary>Конфигурационный файл базы данных, уровень лога и т.д.</summary>
+            public const string Config = "config.xml";          
+        }
+    }
+}
---
+++ .github /API/Константы/FolderName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Constants
+{
+    public static class FolderName
+    {
+        /// <summary>Название папок логов для всех приложений</summary>
+        public const string Logs = "Logs";
+
+        public static class Server
+        {
+            /// <summary>Название папки, в которой хранятся локальные для объекта настройки сервера</summary>
+            public static string LocalConfig = "IBS";
+            /// <summary>Название корневой папки, в которой хранятся данные сервера</summary>
+            public const string Root = "Server";
+        }
+    }
+}
