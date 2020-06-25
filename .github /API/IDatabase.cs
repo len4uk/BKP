16eca130585c0e0ac48423e81068d8b57c365e10
len4uk <lenya1.3@mail.ru>
2020-06-26 02:15:00 +0300

    Add files via upload

---
+++ .github /API/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using API.Data;
+using System.Collections.Generic;
+
+namespace API
+{
+    public interface IDatabase
+    {
+        bool AddUser(string login, string password, string fio, string phone = null, string email = null, string numberCard = null);
+
+        bool CheckUser(string login, string password);
+
+        List<Routes> CheckRouteCity(string city);
+
+        List<RouteTransport> CheckRouteNumber(string city, string number, string mode);
+    }
+}
